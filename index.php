7149cbee5ae898267d2c2a7ee50d71b8da7c6241
Jan Minar <rdancer@rdancer.org>
2013-07-07 04:29:32 +0100

    Decrease the number of reaction videos
    The videos are very CPU-intensive; decrease their number

--- index.php
+++ index.php
@@ -36,26 +36,26 @@
 </head>
 <body>
   <table id="videoMatrix">
-    <tr>
-      <td id="foo" class="reaction">1.1
+    <!--tr>
+      <td class="reaction">1.1
       <td class="reaction">1.2
       <td class="reaction">1.3
       <td class="reaction">1.4
       <td class="reaction">1.5
       <td class="reaction">1.6
       <td class="reaction">1.7
-    </tr>
+    </tr-->
     <tr>
-      <td class="reaction">2.1
+<!--      <td class="reaction">2.1 -->
       <td class="reaction">2.2
       <td class="reaction">2.3
       <td class="reaction">2.4
       <td class="reaction">2.5
       <td class="reaction">2.6
-      <td class="reaction">2.7
+<!--      <td class="reaction">2.7 -->
     </tr>
     <tr>
-      <td class="reaction">3.1
+<!--      <td class="reaction">3.1 -->
       <td class="reaction">3.2
       <td class="content" colspan="3" rowspan="3">
 	<video id="contentVideo" class="span12 video content cool-background row' + rowNumber + ' cell' + cellNumber + ' "" controls="controls">
@@ -64,36 +64,36 @@
       <!--td>3.4
       <td class="reaction">3.5 -->
       <td class="reaction">3.6
-      <td class="reaction">3.7
+<!--      <td class="reaction">3.7 -->
     </tr>
     <tr>
-      <td class="reaction">4.1
+<!--      <td class="reaction">4.1 -->
       <td class="reaction">4.2
       <!--td>4.3
       <td class="reaction">4.4
       <td class="reaction">4.5 -->
       <td class="reaction">4.6
-      <td class="reaction">4.7
+<!--      <td class="reaction">4.7 -->
     </tr>
     <tr>
-      <td class="reaction">5.1
+<!--      <td class="reaction">5.1 -->
       <td class="reaction">5.2
       <!--td>5.3
       <td class="reaction">5.4
       <td class="reaction">5.5 -->
       <td class="reaction">5.6
-      <td class="reaction">5.7
+<!--      <td class="reaction">5.7 -->
     </tr>
     <tr>
-      <td class="reaction">6.1
+<!--      <td class="reaction">6.1 -->
       <td class="reaction">6.2
       <td class="reaction">6.3
       <td class="reaction">6.4
       <td class="reaction">6.5
       <td class="reaction">6.6
-      <td class="reaction">6.7
+<!--      <td class="reaction">6.7 -->
     </tr>
-    <tr>
+    <!--tr>
       <td class="reaction">7.1
       <td class="reaction">7.2
       <td class="reaction">7.3
@@ -101,6 +101,6 @@
       <td class="reaction">7.5
       <td class="reaction">7.6
       <td class="reaction">7.7
-    </tr>
+    </tr-->
   </table id="videoMatrix">
 </body>
